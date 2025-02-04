c344f8fb6c06fa98538ed9458d9269ff3d8cc437
Abhishek <abhishekpooja1408@gmail.com>
2025-02-04 22:31:03 +0530

    Update app.java

--- app.java
+++ app.java
@@ -7,7 +7,6 @@ import java.util.List;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.hmkcode.vo.Article;
-
 public class App  
 {
     public static void main( String[] args )
